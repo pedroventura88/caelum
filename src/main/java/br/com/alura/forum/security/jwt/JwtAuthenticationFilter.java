9000c453748d36a1c94040567550c0439ea8b068
pedro.pasv <paventura@stefanini.com>
2020-06-20 17:17:56 -0300

    Criação da CLasse JwtAuthenticationFilter

--- src/main/java/br/com/alura/forum/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/br/com/alura/forum/security/jwt/JwtAuthenticationFilter.java
@@ -1,16 +1,54 @@
-//package br.com.alura.forum.security.jwt;
-//
-//import org.springframework.web.filter.OncePerRequestFilter;
-//
-//import javax.servlet.FilterChain;
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-//public class JwtAuthenticationFilter extends OncePerRequestFilter {
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) {
-//        String jwt = get
-//    }
-//}
+package br.com.alura.forum.security.jwt;
+
+import antlr.StringUtils;
+import br.com.alura.forum.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@AllArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    public static final String BEARER_PREFIX = "Bearer ";
+
+    private TokenManager tokenManager;
+    private UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest,
+                                    HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain) throws IOException, ServletException {
+
+        String x = httpServletRequest.getHeader("Authorization");
+
+        String token = getJwtToken(x);
+
+        if (tokenManager.isValid(token)) {
+            Long userId = tokenManager.getUserIdFromToken(token);
+            UserDetails user = userService.loadUserById(userId);
+
+            /** Esse metodo abaixo, é oq mantem o usuário autenticado com o TOKEN*/
+            SecurityContextHolder.getContext().setAuthentication(
+                    new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities()));
+        }
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    private String getJwtToken(String bearerToken) {
+        System.out.println("DENTRO !!");
+        String jwt = null;
+        if (bearerToken!=null && bearerToken.startsWith(BEARER_PREFIX)){
+            jwt = bearerToken.replace(BEARER_PREFIX,"");
+        }
+        return jwt;
+    }
+
+}
