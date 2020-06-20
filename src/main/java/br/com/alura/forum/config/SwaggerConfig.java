55e1cb93ff21f8a5a9282ae88da45f3b7e4a7c17
pedro.pasv <paventura@stefanini.com>
2020-06-20 17:19:56 -0300

    Ajustes finais referentes a autorização do Token no Swagger.

--- src/main/java/br/com/alura/forum/config/SwaggerConfig.java
+++ src/main/java/br/com/alura/forum/config/SwaggerConfig.java
@@ -1,16 +1,21 @@
 package br.com.alura.forum.config;
 
+import br.com.alura.forum.model.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 import static springfox.documentation.builders.PathSelectors.ant;
 
@@ -36,7 +41,18 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("br.com.alura.forum.controller"))
                 .paths(ant("/api/**"))
                 .build()
-                .apiInfo(informacoes);
+                .apiInfo(informacoes)
+                .ignoredParameterTypes(User.class)
+                .globalOperationParameters(
+                        Arrays.asList(
+                                new ParameterBuilder()
+                                .name("Authorization")
+                                .description("Header para facilitar o envio do Authorization Bearer Token")
+                                .modelRef(new ModelRef("string"))
+                                .parameterType("header")
+                                .required(false)
+                                .build())
+                );
     }
 
 }
